54aaa205ea832a570a75b3ba5a88411328bcb58f
Dena Aghajari <d.aghajari1234@gmail.com>
2021-06-18 12:42:58 +0200

    todo person test

---
+++ src/test/java/se/lexicon/data/PersonSequencerTest.java
@@ -0,0 +1,17 @@
+package se.lexicon.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonSequencerTest {
+
+    @Test
+    public void nextPersonId() {
+
+    }
+
+    @Test
+    public void reset() {
+    }
+}
--- src/test/java/se/lexicon/model/PersonTest.java
+++ src/test/java/se/lexicon/model/PersonTest.java
@@ -1,9 +1,30 @@
 package se.lexicon.model;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PersonTest {
 
 
 
+
+    @Test
+   public void successfully_created(){
+        //Arrange
+        int counter=0;
+        Person test=null;
+        String expectedFirstName="testName";
+        String expectedLastName="testLastName";
+        String expectedEmail="d.ag@gmail.com";
+        int personId=++counter;
+        //Act
+        Person testObject=new Person("testName","testLastName","d.ag@gmail.com");
+        //Assert
+        assertEquals("testName",testObject.getFirstName());
+        assertEquals("testLastName",testObject.getLastName());
+        assertEquals("d.ag@gmail.com",testObject.getEmail());
+        System.out.println("person ID is:"+Person.counter);
+
+    }
 }
