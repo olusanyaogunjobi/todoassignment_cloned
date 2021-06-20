7843e86226b3b5696e26d5ad65e8ceea806198c8
Dena Aghajari <d.aghajari1234@gmail.com>
2021-06-20 12:20:30 +0200

    todo person test

--- src/main/java/se/lexicon/data/TodoSequencer.java
+++ src/main/java/se/lexicon/data/TodoSequencer.java
@@ -1,5 +1,15 @@
 package se.lexicon.data;
 
 public class TodoSequencer {
+    private static int toDoId=0;
+
+    public static int nextTodoId(){
+        return ++toDoId;
+    }
+    public static void reset(){
+        toDoId=0;
+    }
+
+
 
 }
